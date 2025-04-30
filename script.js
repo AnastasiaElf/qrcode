12a29fe36d862bbe0d112b81aa4ecbc6ea342ba3
AnastasiaElf <anastasia.elf.17@gmail.com>
2025-04-30 17:21:30 +0200

    add advanced options

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 const default_size = 2000;
-const default_margin_percent = 3;
-const default_logo_margin_percent = 2;
 const url_placeholder = "Paste your link here";
 const logo_text_placeholder = '"Scan Me!"';
 const logo_image_placeholder = "images/logo_placeholder.webp";
@@ -39,6 +37,23 @@ const radioConfig = {
     },
 };
 
+const numberConfig = {
+    input_margin: {
+        defaultValue: 3,
+        step: 1,
+        min: 0,
+        max: 10,
+        placeholder: "No margin",
+    },
+    input_logo_margin: {
+        defaultValue: 2,
+        step: 1,
+        min: 0,
+        max: 5,
+        placeholder: "No margin",
+    },
+};
+
 let qrCode;
 
 function downloadQR(extension) {
@@ -57,13 +72,18 @@ function updateQR() {
     const dotStyle = document.querySelector('input[name="input_dot_style"]:checked').value;
     const cornerSquareStyle = document.querySelector('input[name="input_corner_square_shape"]:checked').value;
     const cornerDotStyle = document.querySelector('input[name="input_corner_dot_shape"]:checked').value;
+    let margin = parseInt(document.getElementById("input_margin").value);
+    let imageMargin = parseInt(document.getElementById("input_logo_margin").value);
 
     if (!url) url = url_placeholder;
     if (!logo_text) logo_text = logo_text_placeholder;
+    if (!margin) margin = 0;
+    if (!imageMargin) imageMargin = 0;
 
     qrCode.update({
         data: url,
         shape: shape,
+        margin: (margin / 100) * default_size,
         dotsOptions: {
             type: dotStyle,
         },
@@ -72,6 +92,10 @@ function updateQR() {
         },
         cornersDotOptions: {
             type: cornerDotStyle,
+        },
+        imageOptions: {
+            margin: (imageMargin / 100) * default_size,
+            imageSize: 0.5,
         },
     });
 }
@@ -148,6 +172,40 @@ function setupRadioInputs(config) {
     }
 }
 
+function setupNumberInputs(config) {
+    for (const key in config) {
+        const elem = document.getElementById(key);
+        if (!elem || elem.type !== "number") continue;
+
+        const params = config[key];
+        if ("min" in params) elem.min = params.min;
+        if ("max" in params) elem.max = params.max;
+        if ("step" in params) elem.step = params.step;
+        if ("placeholder" in params) elem.placeholder = params.placeholder;
+        if ("defaultValue" in params) elem.value = params.defaultValue;
+
+        elem.oninput =
+            config.onChange ||
+            (() => {
+                let value = parseInt(elem.value);
+                if (value && (value < params.min || value > params.max)) {
+                    // TODO: error message
+                } else {
+                    updateQR();
+                }
+            });
+        if (params.onSetup) params.onSetup(elem, params);
+    }
+}
+
+function setupAdvancedParamsCollapse() {
+    document.querySelector(".advanced-params-toggle").addEventListener("click", (e) => {
+        e.target.classList.toggle("active");
+        const content = e.target.nextElementSibling;
+        content.style.maxHeight = content.style.maxHeight ? "" : content.scrollHeight + "px";
+    });
+}
+
 function getInitialValues() {
     const url = textConfig.input_url.defaultValue || url_placeholder;
     const logo_text = textareaConfig.input_logo_text.defaultValue || logo_text_placeholder;
@@ -155,12 +213,15 @@ function getInitialValues() {
     const dotStyle = radioConfig.input_dot_style.defaultValue;
     const cornerSquareStyle = radioConfig.input_corner_square_shape.defaultValue;
     const cornerDotStyle = radioConfig.input_corner_dot_shape.defaultValue;
+    const margin = numberConfig.input_margin.defaultValue;
+    const imageMargin = numberConfig.input_logo_margin.defaultValue;
 
-    return { url, logo_text, shape, dotStyle, cornerSquareStyle, cornerDotStyle };
+    return { url, logo_text, shape, dotStyle, cornerSquareStyle, cornerDotStyle, margin, imageMargin };
 }
 
 function initQRCode() {
-    const { url, logo_text, shape, dotStyle, cornerSquareStyle, cornerDotStyle } = getInitialValues();
+    const { url, logo_text, shape, dotStyle, cornerSquareStyle, cornerDotStyle, margin, imageMargin } =
+        getInitialValues();
 
     qrCode = new QRCodeStyling({
         width: default_size,
@@ -168,8 +229,7 @@ function initQRCode() {
         type: "svg",
         data: url,
         shape: shape,
-        image: logo_image_placeholder,
-        margin: (default_margin_percent / 100) * default_size,
+        margin: (margin / 100) * default_size,
         backgroundOptions: {
             color: null,
         },
@@ -182,8 +242,9 @@ function initQRCode() {
         cornersDotOptions: {
             type: cornerDotStyle,
         },
+        image: logo_image_placeholder,
         imageOptions: {
-            margin: (default_logo_margin_percent / 100) * default_size,
+            margin: (imageMargin / 100) * default_size,
             imageSize: 0.5,
         },
     });
@@ -195,6 +256,8 @@ function initApp() {
     setupTextInputs(textConfig);
     setupTextareaInputs(textareaConfig);
     setupRadioInputs(radioConfig);
+    setupNumberInputs(numberConfig);
+    setupAdvancedParamsCollapse();
     initQRCode();
 }
 
